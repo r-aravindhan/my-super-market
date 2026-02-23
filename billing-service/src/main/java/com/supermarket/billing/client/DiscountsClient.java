3ff910254d05ee19f9962eb716aba9e71b29b291
Aravindhan <aravindhan_ravichandran@epam.com>
2026-02-23 20:00:04 +0530

    Refactor DiscountsClient to use Lombok's @RequiredArgsConstructor for dependency injection, simplifying constructor code.

--- billing-service/src/main/java/com/supermarket/billing/client/DiscountsClient.java
+++ billing-service/src/main/java/com/supermarket/billing/client/DiscountsClient.java
@@ -2,21 +2,20 @@ package com.supermarket.billing.client;
 
 import com.supermarket.billing.dto.DiscountApplicationRequest;
 import com.supermarket.billing.dto.DiscountApplicationResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
+@RequiredArgsConstructor
 public class DiscountsClient {
 
     private static final String APPLY_PATH = "/api/v1/discounts/apply";
 
+    @Qualifier("discountsRestClient")
     private final RestClient restClient;
-
-    public DiscountsClient(@Qualifier("discountsRestClient") RestClient restClient) {
-        this.restClient = restClient;
-    }
 
     public DiscountApplicationResponse applyDiscount(DiscountApplicationRequest request) {
         return restClient.post()
